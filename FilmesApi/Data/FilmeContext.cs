a7e53d20d1c607939616d4529688ee54b5893594
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2022-01-26 19:28:21 -0300

    adicionando migrations e dbContext

---
+++ FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,14 @@
+using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
